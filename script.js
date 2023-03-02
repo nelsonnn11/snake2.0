50a0ce6cf2d82bfdebf5c2261526a8fd6c329ab0
Nelson <124norellana@frhsd.com>
2023-03-02 10:57:37 -0500

    audio

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ const initGame = () => {
     // Checking if the snake's head is out of wall, if so setting gameOver to true
     if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         return gameOver = true;
+        var audio = new Audio ("audio/fail.mp3")
+        audio.play();
     }
     for (let i = 0; i < snakeBody.length; i++) {
         // Adding a div for each part of the snake's body
@@ -80,3 +82,4 @@ const initGame = () => {
 updateFoodPosition();
 setIntervalId = setInterval(initGame, 100);
 document.addEventListener("keyup", changeDirection)
+
